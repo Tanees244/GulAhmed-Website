1d946f8287ddf46a562d1ba7dde0c938a05a057f
Tanees <k201879@nu.edu.pk>
2023-10-14 19:09:40 +0500

    tanees

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -1,30 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { db } from './firebase'; // Import the Firebase Firestore reference
+import { collection, getDocs } from 'firebase/firestore'; // Add these import statements
 
-function DataDisplay() {
+function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Get Firestore instance
-    const db = getFirestore();
-
-    // Get Auth instance
-    const auth = getAuth();
-
     const fetchData = async () => {
-      const dataRef = collection(db, 'users'); // Replace with your Firestore collection name
+      const dataRef = collection(db, 'firestore'); // Replace with your Firestore collection name
 
       try {
-        const querySnapshot = await getDocs(dataRef);
-
-        const data = querySnapshot.docs.map((doc) => ({
+        const snapshot = await getDocs(dataRef);
+        const data = snapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
+
         setData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+      } catch (err) {
+        console.error('Error:', err);
       }
     };
 
@@ -33,14 +27,14 @@ function DataDisplay() {
 
   return (
     <div>
-      <h1>Data from Firestore</h1>
+      <h1>My Firestore Data</h1>
       <ul>
         {data.map((item) => (
-          <li key={item.id}>{item.name}</li> {/* Modify this based on your data structure */}
+          <li key={item.id}>{item.name}</li>
         ))}
       </ul>
     </div>
   );
 }
 
-export default DataDisplay;
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCn00GG23owlGLiE7xE-R2JoVPtjRoC9go",
+    authDomain: "jsonwebsite-e0bdb.firebaseapp.com",
+    projectId: "jsonwebsite-e0bdb",
+    storageBucket: "jsonwebsite-e0bdb.appspot.com",
+    messagingSenderId: "66560867708",
+    appId: "1:66560867708:web:b82afb4d7bc4ff6b67b8f9",
+    measurementId: "G-J5DKK6P06M"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); 
+
+export { db };
