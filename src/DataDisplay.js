07955db097e039dbdb60a0df7a74a2d8467cd0c3
Tanees <k201879@nu.edu.pk>
2023-10-15 19:12:31 +0500

    tanees

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -1,38 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { db } from './firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore';
 
 function DataDisplay() {
   const [data, setData] = useState([]);
+  const [sortOption, setSortOption] = useState('priceAsc');
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dataRef = collection(db, 'products');
-        const querySnapshot = await getDocs(dataRef);
+    fetchData(sortOption);
+  }, [sortOption]);
 
-        const documents = [];
-        querySnapshot.forEach((doc) => {
-          documents.push({ id: doc.id, ...doc.data() });
-        });
-
-        setData(documents);
-      } catch (error) {
-        console.error('Error fetching data from Firestore:', error);
+  const fetchData = async (sortOption) => {
+    try {
+      const dataRef = collection(db, 'products');
+      let sortField;
+      let sortOrder = 'asc'; // Default to ascending
+  
+      if (sortOption === 'priceAsc') {
+        sortField = 'newPrice';
+        sortOrder = 'asc';
+      } else if (sortOption === 'priceDesc') {
+        sortField = 'newPrice';
+        sortOrder = 'desc';
+      } else if (sortOption === 'discountAsc') {
+        sortField = 'discount';
+        sortOrder = 'asc';
+      } else if (sortOption === 'discountDesc') {
+        sortField = 'discount';
+        sortOrder = 'desc';
+      } else {
+        sortField = 'dateAdded'; // Sort by date added
+        sortOrder = 'asc';
       }
-    };
-
-    fetchData();
-  }, [db]);
+  
+      const q = query(dataRef, orderBy(sortField, sortOrder), limit(12)); // Limit to 12 records
+      const querySnapshot = await getDocs(q);
+  
+      const documents = [];
+      querySnapshot.forEach((doc) => {
+        documents.push({ id: doc.id, ...doc.data() });
+      });
+  
+      setData(documents);
+    } catch (error) {
+      console.error('Error fetching data from Firestore:', error);
+    }
+  };
 
   return (
     <div>
       <h1>Firebase Data</h1>
+      <div>
+        <button onClick={() => setSortOption('priceAsc')}>Sort by Price (Asc)</button>
+        <button onClick={() => setSortOption('priceDesc')}>Sort by Price (Desc)</button>
+        <button onClick={() => setSortOption('discountAsc')}>Sort by Discount (Asc)</button>
+        <button onClick={() => setSortOption('discountDesc')}>Sort by Discount (Desc)</button>
+        <button onClick={() => setSortOption('dateAdded')}>Sort by Date Added</button>
+      </div>
       <ul>
         {data.map((item) => (
           <li key={item.id}>
-            <p>Name: {item.productID}</p>
+            <p>Name: {item.brand}</p>
             <p>Price: {item.newPrice}</p>
+            <p>Discount: {item.discount}</p>
+            <p>Date Added: {item.dateAdded}</p>
           </li>
         ))}
       </ul>
